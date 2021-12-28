7e68efe13b15726dcc54fdc55e12ff4a58ef36dd
Kevin Matthes <kevin.matthes@s2019.tu-chemnitz.de>
2021-12-28 18:06:19 +0100

    Adjust build steps

--- .docs/tidy.m
+++ .docs/tidy.m
@@ -66,10 +66,37 @@ PDFTODELETE = 0;
 %%
 %%%%
 
-% Remove the manual.
-[status ~]  = system ('test -e *.pdf');
-if ~ status;
-    delete ('*.pdf');
+% Information banner.
+fprintf ('%%%%%%%% Begin documentation clean-up. %%%%%%%%\n\n');
+
+% Determine the operating system.
+fprintf ('Determine OS: ');
+if isunix;
+    UNIXCMDS = 1;
+    disp ('UNIX.');
+elseif ispc;
+    disp ('Windows.');
 end;
+
+% Check for files to delete.
+if UNIXCMDS;
+    disp ('Check for documentation artifacts to remove ...');
+    PDFTODELETE = ~ unix ('test -e *.pdf');
+
+    if PDFTODELETE;
+        disp ('PDF artifacts found!');
+    else;
+        disp ('No PDF artifacts found.');
+    end;
+end;
+
+% Remove all artifacts.
+if PDFTODELETE;
+    delete ('*.pdf');
+    disp ('PDF artifacts removed.');
+end;
+
+% Leave the script.
+fprintf ('\n%%%%%%%% End documentation clean-up. %%%%%%%%\n');
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
